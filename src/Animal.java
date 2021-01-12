b3466797281c961a63223b41711de1315352f80e
RicardoM <ricardomorganho@gmail.com>
2021-01-12 15:19:53 +0000

    classe animal adicionada

---
+++ src/Animal.java
@@ -0,0 +1,26 @@
+public class Animal {
+
+    private String nome;
+    private int patas;
+
+    public Animal(String nome, int patas) {
+        this.nome = nome;
+        this.patas = patas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
+}
