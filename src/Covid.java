86287077d4094e4bb374b7f4c7d2fafba5d6a55f
Joana Antunes <antunesjoana@live.com.pt>
2021-01-12 16:18:29 +0000

    Covid Primavera/Verão 2021

--- src/Covid.java
+++ src/Covid.java
@@ -1,29 +1,22 @@
+public class Covid {
 
-    public class Covid {
 
-        private double salario;
-        private String nome;
-        private int idade;
+    private String nome;
+    private int idade;
 
-        public Covid(String nome, int idade) {
-            this.nome = nome;
-            this.idade = idade;
-            salario = 800;
-        }
+    public Covid(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
 
-        public double getSalario() {
-            return salario;
-        }
-
-        public String getNome() {
-            return nome;
-        }
-
-        public int getIdade() {
-            return idade;
-        }
     }
 
 
+    public String getNome() {
+        return nome;
+    }
 
+    public int getIdade() {
+        return idade;
+    }
+}
 
