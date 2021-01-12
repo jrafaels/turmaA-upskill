6ca6314c31f35c64b65570d001478530d011c9aa
Joana Antunes <antunesjoana@live.com.pt>
2021-01-12 16:04:27 +0000

    Classe Funcionario sem Funcionalidade

---
+++ src/Covid.java
@@ -0,0 +1,29 @@
+
+    public class Covid {
+
+        private double salario;
+        private String nome;
+        private int idade;
+
+        public Covid(String nome, int idade) {
+            this.nome = nome;
+            this.idade = idade;
+            salario = 800;
+        }
+
+        public double getSalario() {
+            return salario;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public int getIdade() {
+            return idade;
+        }
+    }
+
+
+
+
